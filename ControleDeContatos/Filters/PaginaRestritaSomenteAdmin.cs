082a38684baad0245e0aed5ad4cf45f91a339d98
Ryan de Menezes <menezesryan1010@gmail.com>
2025-01-21 19:49:28 -0300

    criando os filtros da aplicação

---
+++ ControleDeContatos/Filters/PaginaRestritaSomenteAdmin.cs
@@ -0,0 +1,30 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ControleDeContatos.Enum;
+
+namespace ControleDeContatos.Filters
+{
+    public class PaginaRestritaSomenteAdmin : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            string? sessaoUsuario = context.HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if (string.IsNullOrEmpty(sessaoUsuario))
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+            }
+            else
+            {
+                var usuario = JsonConvert.DeserializeObject<UsuarioModel>(sessaoUsuario);
+
+                if (usuario == null || usuario.Perfil != PerfilEnum.Admin)
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Restrito" }, { "action", "Index" } });
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
